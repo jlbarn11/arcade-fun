77a0e8d2951db81aad87f4eaf702cb321eae53ab
Jessica Barnett <jlbarn11@asu.edu>
2023-05-17 10:41:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    scene.setBackgroundColor(7)
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(5)
+})
+scene.setBackgroundColor(1)
